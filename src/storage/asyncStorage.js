a70919ca3082de1c656bdbc090d6f646b08b7be0
artem.malieiev <artem.malieiev@exclaimer.com>
2022-05-16 15:26:56 +0300

    added wraper function

--- src/storage/asyncStorage.js
+++ src/storage/asyncStorage.js
@@ -1,34 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import MainStack from '../../navigate';
-import LanguagesSelect from '../menu/languagesSelect'
+import LanguagesSelect from '../menu/languagesSelect';
 
+export default function AsyncStorageGetItem({gamer}) {
+    const [storage, setStorage] = useState(gamer);
 
+    useEffect(() => {
+        (async () => {
+            try {
+                const jsonValue = await AsyncStorage.getItem('objGameStore');
+                const value = jsonValue ? JSON.parse(jsonValue) : null;
+                setStorage(value);
+            } catch (e) {
+                // error reading value
+            }
+        })();
+    }, []);
 
-
-export default function AsyncStorageGetItem({ gamer }) {
-
-
-  const [ storage , setStorage ] = useState (gamer);
-
-
-  useEffect( async () => {
-    debugger
-    try {
-      const jsonValue = await AsyncStorage.getItem('objGameStore')
-      const value = jsonValue  ? JSON.parse(jsonValue) : null;
-      debugger
-      setStorage (value)
-
-      debugger
-    } catch (e) {
-      // error reading value
-    }
-  }, []);
-
-  debugger
-
- return storage.leng ? <MainStack gamer={storage} /> : <LanguagesSelect gamer={storage} /> ;
-
-
+    return storage.leng ? (
+        <MainStack gamer={storage} />
+    ) : (
+        <LanguagesSelect gamer={storage} />
+    );
 }
